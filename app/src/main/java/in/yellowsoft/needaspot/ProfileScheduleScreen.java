5a1960b605f88c19e3b1c3fdf024cada83c78794
Sravani <sravani@yellowsoft.in>
2016-12-22 17:25:30 +0530

    hello

---
+++ app/src/main/java/in/yellowsoft/needaspot/ProfileScheduleScreen.java
@@ -0,0 +1,15 @@
+package in.yellowsoft.needaspot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by T on 22-12-2016.
+ */
+
+public class ProfileScheduleScreen extends Activity {
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_schedule_screen);
+    }
+}
